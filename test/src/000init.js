6ab90dc9a2259094e71604d90bd446717906131f
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-03-29 10:16:55 +0900

    test: nit

--- test/src/000init.js
+++ test/src/000init.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import {taffy} from 'taffydb';
 import esdoc from './../../src/esdoc.js';
+import defaultPublisher from '../../src/Publisher/publish.js';
 
 let configFilePath = './test/fixture/esdoc.json';
 let configJSON = fs.readFileSync(configFilePath, {encode: 'utf8'});
 let config = JSON.parse(configJSON);
 
-esdoc(config, (data)=>{
+esdoc(config, (data, config)=>{
   let db = taffy(data);
   db.find = function(...cond) {
     return db(...cond).map((v)=>{
@@ -17,4 +18,9 @@ esdoc(config, (data)=>{
     });
   };
   global.db = db;
+
+  console.log(db.find({kind: 'class'}));
+  console.log(db.find({name: '_p1'}));
+
+  defaultPublisher(data, config);
 });
