aa2f183d69c4af98a139f4f229220d6445998b70
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-05 21:26:08 +0900

    refactor: MethodDoc extends FunctionDoc.

--- src/Doc/FunctionDoc.js
+++ src/Doc/FunctionDoc.js
@@ -1,5 +1,5 @@
 import AbstractDoc from './AbstractDoc.js';
-import MethodDoc from './MethodDoc.js';
+import ParamParser from '../Parser/ParamParser.js';
 
 export default class FunctionDoc extends AbstractDoc {
   _apply() {
@@ -8,6 +8,12 @@ export default class FunctionDoc extends AbstractDoc {
     this['@param']();
     this['@property']();
     this['@return']();
+    this['@type']();
+    this['@abstract']();
+    this['@override']();
+    this['@throws']();
+    this['@emits']();
+    this['@listens']();
   }
 
   ['@kind']() {
@@ -15,12 +21,6 @@ export default class FunctionDoc extends AbstractDoc {
     if (this._value.kind) return;
     this._value.kind = 'function';
   }
-
-  //['@static']() {
-  //  super['@static']();
-  //  if ('static' in this._value) return;
-  //  this._value.static = true;
-  //}
 
   ['@name']() {
     super['@name']();
@@ -35,14 +35,103 @@ export default class FunctionDoc extends AbstractDoc {
   }
 
   ['@param']() {
-    MethodDoc.prototype['@param'].call(this);
-  }
+    let values = this._findAllTagValues(['@param']);
+    if (!values) return;
 
-  ['@property']() {
-    MethodDoc.prototype['@property'].call(this);
+    this._value.params = [];
+    for (let value of values) {
+      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+      this._value.params.push(result);
+    }
   }
 
   ['@return']() {
-    MethodDoc.prototype['@return'].call(this);
+    let value = this._findTagValue(['@return', '@returns']);
+    if (!value) return;
+
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    this._value.return = result;
+  }
+
+  ['@property']() {
+    let values = this._findAllTagValues(['@property']);
+    if (!values) return;
+
+    this._value.properties = [];
+    for (let value of values) {
+      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+      this._value.properties.push(result);
+    }
+  }
+
+  ['@type']() {
+    let value = this._findTagValue(['@type']);
+    if (!value) return;
+
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, false);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    this._value.type = result;
+  }
+
+  ['@abstract']() {
+    let tag = this._find(['@abstract']);
+    if (tag) {
+      this._value.abstract = true;
+    }
+  }
+
+  ['@override'](){
+    let tag = this._find(['@override']);
+    if (tag) {
+      this._value.override = true;
+    }
+  }
+
+  ['@throws'](){
+    let values = this._findAllTagValues(['@throws']);
+    if (!values) return;
+
+    this._value.throws = [];
+    for (let value of values) {
+      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
+      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+      this._value.throws.push({
+        types: result.types,
+        description: result.description
+      });
+    }
+  }
+
+  ['@emits'](){
+    let values = this._findAllTagValues(['@emits']);
+    if (!values) return;
+
+    this._value.emits = [];
+    for (let value of values) {
+      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
+      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+      this._value.emits.push({
+        types: result.types,
+        description: result.description
+      });
+    }
+  }
+
+  ['@listens'](){
+    let values = this._findAllTagValues(['@listens']);
+    if (!values) return;
+
+    this._value.listens = [];
+    for (let value of values) {
+      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
+      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+      this._value.listens.push({
+        types: result.types,
+        description: result.description
+      });
+    }
   }
 }
--- src/Doc/MethodDoc.js
+++ src/Doc/MethodDoc.js
@@ -1,19 +1,9 @@
 import AbstractDoc from './AbstractDoc.js';
-import ParamParser from '../Parser/ParamParser.js';
+import FunctionDoc from './FunctionDoc.js';
 
-export default class MethodDoc extends AbstractDoc {
+export default class MethodDoc extends FunctionDoc {
   _apply() {
     super._apply();
-
-    this['@param']();
-    this['@property']();
-    this['@return']();
-    this['@type']();
-    this['@abstract']();
-    this['@override']();
-    this['@throws']();
-    this['@emits']();
-    this['@listens']();
 
     delete this._value.export;
     delete this._value.importPath;
@@ -21,19 +11,19 @@ export default class MethodDoc extends AbstractDoc {
   }
 
   ['@kind']() {
-    super['@kind']();
+    AbstractDoc.prototype['@kind'].call(this);
     if (this._value.kind) return;
     this._value.kind = this._node.kind;
   }
 
   ['@name']() {
-    super['@name']();
+    AbstractDoc.prototype['@name'].call(this);
     if (this._value.name) return;
     this._value.name = this._node.key.name;
   }
 
   ['@memberof']() {
-    super['@memberof']();
+    AbstractDoc.prototype['@memberof'].call(this);
     if (this._value.memberof) return;
 
     let memberof;
@@ -45,107 +35,6 @@ export default class MethodDoc extends AbstractDoc {
         return;
       }
       parent = parent.parent;
-    }
-  }
-
-  ['@param']() {
-    let values = this._findAllTagValues(['@param']);
-    if (!values) return;
-
-    this._value.params = [];
-    for (let value of values) {
-      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
-      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-      this._value.params.push(result);
-    }
-  }
-
-  ['@return']() {
-    let value = this._findTagValue(['@return', '@returns']);
-    if (!value) return;
-
-    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
-    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-    this._value.return = result;
-  }
-
-  ['@property']() {
-    let values = this._findAllTagValues(['@property']);
-    if (!values) return;
-
-    this._value.properties = [];
-    for (let value of values) {
-      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
-      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-      this._value.properties.push(result);
-    }
-  }
-
-  ['@type']() {
-    let value = this._findTagValue(['@type']);
-    if (!value) return;
-
-    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, false);
-    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-    this._value.type = result;
-  }
-
-  ['@abstract']() {
-    let tag = this._find(['@abstract']);
-    if (tag) {
-      this._value.abstract = true;
-    }
-  }
-
-  ['@override'](){
-    let tag = this._find(['@override']);
-    if (tag) {
-      this._value.override = true;
-    }
-  }
-
-  ['@throws'](){
-    let values = this._findAllTagValues(['@throws']);
-    if (!values) return;
-
-    this._value.throws = [];
-    for (let value of values) {
-      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
-      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-      this._value.throws.push({
-        types: result.types,
-        description: result.description
-      });
-    }
-  }
-
-  ['@emits'](){
-    let values = this._findAllTagValues(['@emits']);
-    if (!values) return;
-
-    this._value.emits = [];
-    for (let value of values) {
-      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
-      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-      this._value.emits.push({
-        types: result.types,
-        description: result.description
-      });
-    }
-  }
-
-  ['@listens'](){
-    let values = this._findAllTagValues(['@listens']);
-    if (!values) return;
-
-    this._value.listens = [];
-    for (let value of values) {
-      let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
-      let result = ParamParser.parseParam(typeText, paramName, paramDesc);
-      this._value.listens.push({
-        types: result.types,
-        description: result.description
-      });
     }
   }
 }
