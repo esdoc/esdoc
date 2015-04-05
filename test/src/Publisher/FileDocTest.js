bc9e36aec72c353825f80c7bcff86887b038ca05
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-05 22:09:39 +0900

    test: file.

---
+++ test/src/Publisher/FileDocTest.js
@@ -0,0 +1,10 @@
+import {readDoc, assert, find} from './util.js';
+
+describe('MyClass.js.html:', ()=> {
+  let doc = readDoc('@file-src|MyClass.js.html');
+
+  it('has source code.', ()=>{
+    assert.includes(doc, '[data-ice="title"]', 'src/MyClass.js');
+    assert.includes(doc, 'code[data-ice="content"]', 'export default class MyClass1 extends SuperMyClass1');
+  });
+});
