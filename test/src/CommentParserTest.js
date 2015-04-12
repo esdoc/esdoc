a32a36dca8913f0406bc17b2f5b61d6d6bd9ed3b
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-12 11:26:29 +0900

    test: add parser test.

---
+++ test/src/CommentParserTest.js
@@ -0,0 +1,41 @@
+import assert from 'power-assert';
+import CommentParser from '../../src/Parser/CommentParser.js';
+
+describe('CommentParser:', ()=>{
+  it('can parse doc comment.', ()=>{
+    let value = `*
+* this is desc.
+* @tag1
+* @tag2 tag2 value
+* @tag3 tag3 value
+* tag3 second value
+*
+* @tag4 tag4 value
+*
+`;
+    let comment = {type: 'Block', value: value};
+    let tags = CommentParser.parse(comment);
+    assert.equal(tags.length, 5);
+    assert.deepEqual(tags[0], {tagName: '@desc', tagValue: 'this is desc.'});
+    assert.deepEqual(tags[1], {tagName: '@tag1', tagValue: ''});
+    assert.deepEqual(tags[2], {tagName: '@tag2', tagValue: 'tag2 value'});
+    assert.deepEqual(tags[3], {tagName: '@tag3', tagValue: 'tag3 value\ntag3 second value'});
+    assert.deepEqual(tags[4], {tagName: '@tag4', tagValue: 'tag4 value'});
+  });
+
+  it('return empty with non doc comment.', ()=>{
+    let value = `\
+this is not doc comment.
+`;
+    let comment = {type: 'Block', value: value};
+    let tags = CommentParser.parse(comment);
+    assert.equal(tags.length, 0);
+  });
+
+  it('return empty with line comment.', ()=>{
+    let value = `this is line comment.`;
+    let comment = {type: 'Block', value: value};
+    let tags = CommentParser.parse(comment);
+    assert.equal(tags.length, 0);
+  });
+});
---
+++ test/src/ParamParserTest.js
@@ -0,0 +1,102 @@
+import assert from 'power-assert';
+import ParamParser from '../../src/Parser/ParamParser.js';
+
+describe('ParamParser:', ()=>{
+  it('parse param value.', ()=>{
+    let value = '{number} p1 this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    assert.equal(typeText, 'number');
+    assert.equal(paramName, 'p1');
+    assert.equal(paramDesc, 'this is desc');
+  });
+
+  it('parse param value with hyphen prefix.', ()=>{
+    let value = '{number} p1 - this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    assert.equal(typeText, 'number');
+    assert.equal(paramName, 'p1');
+    assert.equal(paramDesc, 'this is desc');
+  });
+
+  it('parse param value without param name', ()=>{
+    let value = '{number} this is desc';
+    let {typeText, paramDesc} = ParamParser.parseParamValue(value, true, false, true);
+    assert.equal(typeText, 'number');
+    assert.equal(paramDesc, 'this is desc');
+  });
+
+  it('parse param value without param desc', ()=>{
+    let value = '{number} p1';
+    let {typeText, paramName} = ParamParser.parseParamValue(value, true, true, false);
+    assert.equal(typeText, 'number');
+    assert.equal(paramName, 'p1');
+  });
+
+  it('parse param value with complex', ()=>{
+    let value = '{!(number|string|boolean[])} [p1=10] this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    assert.equal(typeText, '!(number|string|boolean[])');
+    assert.equal(paramName, '[p1=10]');
+    assert.equal(paramDesc, 'this is desc');
+  });
+
+  it('parse param.', ()=>{
+    let value = '{number} p1 this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    assert.deepEqual(result, {
+      nullable: null,
+      types: ['number'],
+      spread: false,
+      optional: false,
+      name: 'p1',
+      description: 'this is desc'
+    });
+  });
+
+  it('parse param with complex.', ()=>{
+    let value = '{?(number|string|boolean[])} [p1] this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    assert.deepEqual(result, {
+      nullable: true,
+      types: ['number', 'string', 'boolean[]'],
+      spread: false,
+      optional: true,
+      name: 'p1',
+      description: 'this is desc',
+    });
+  });
+
+  it('parse param with complex.', ()=>{
+    let value = '{!(number|string|boolean[])} [p1=10] this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    assert.deepEqual(result, {
+      nullable: false,
+      types: ['number', 'string', 'boolean[]'],
+      spread: false,
+      optional: true,
+      name: 'p1',
+      description: 'this is desc',
+      defaultValue: '10',
+      defaultRaw: 10
+    });
+  });
+
+  it('parse param with complex.', ()=>{
+    let value = '{...number} [p1=[10,20,30]] this is desc';
+    let {typeText, paramName, paramDesc} = ParamParser.parseParamValue(value);
+    let result = ParamParser.parseParam(typeText, paramName, paramDesc);
+    assert.deepEqual(result, {
+      nullable: null,
+      types: ['...number'],
+      spread: true,
+      optional: true,
+      name: 'p1',
+      description: 'this is desc',
+      defaultValue: '[10,20,30]',
+      defaultRaw: [10, 20, 30]
+    });
+  });
+});
