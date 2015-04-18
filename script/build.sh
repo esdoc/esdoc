e9a1bb3f1b0ea5499dd8c26c627dcfe73fe789ff
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-18 20:55:54 +0900

    feat: create bin/esdoc

--- script/build.sh
+++ script/build.sh
@@ -5,6 +5,7 @@ rm -rf out/src
 mkdir -p out/src
 ./node_modules/.bin/babel --out-dir out/src src
 chmod +x out/src/ESDocCLI.js
+(mkdir -p ./bin && cd ./bin && rm -rf ./esdoc && ln -s ../out/src/ESDocCLI.js ./esdoc)
 cp -af ./src/Publisher/builder/template ./out/src/Publisher/builder/
 
 # build test
