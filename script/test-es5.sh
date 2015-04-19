e1eede7c3665b0b7e500711673ef730dcb59f77f
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-19 16:08:56 +0900

    chore: does not send coverage in io.js

--- script/test-es5.sh
+++ script/test-es5.sh
@@ -6,7 +6,14 @@ mocha_option="-t 5000 --require ./test/src/espower-loader.js --recursive ./out/t
 
 if [ "$TRAVIS" == "1" ]
 then
-    ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- $mocha_option && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+    # if node is realy node, send coverage. but if node is io.js, does not send coverage.
+    node -v | grep 'v0.12' > /dev/null
+    if [ $? -eq 0 ]
+    then
+        ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- $mocha_option && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+    else
+        ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha  -- $mocha_option
+    fi
 elif [ "$1" == "coverage" ]
 then
     ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha  -- $mocha_option
