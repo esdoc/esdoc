e5746aafca77190155cf9c858248310ef402d00d
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-25 04:07:28 +0900

    feat: show coverage badge in README

--- src/Publisher/builder/ReadmeDocBuilder.js
+++ src/Publisher/builder/ReadmeDocBuilder.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import cheerio from 'cheerio';
 import IceCap from 'ice-cap';
 import DocBuilder from './DocBuilder.js';
 import {markdown} from './util.js';
@@ -24,6 +25,31 @@ export default class ReadmeDocBuilder extends DocBuilder {
     let ice = new IceCap(html);
     ice.load('readme', markdown(readme));
 
-    return ice;
+    let result = ice.html;
+    if (this._config.coverage) {
+      let $ = cheerio.load(result);
+      $('.esdoc-coverage').html(this._buildCoverageHTML());
+      result = $.root().html();
+    }
+
+    return result;
+  }
+
+  _buildCoverageHTML() {
+    let coverage = Math.floor(100 * this._coverage.actualCount / this._coverage.expectCount);
+    let colorClass;
+    if (coverage < 50) {
+      colorClass = 'esdoc-coverage-low';
+    } else if (coverage < 90) {
+      colorClass = 'esdoc-coverage-middle';
+    } else {
+      colorClass = 'esdoc-coverage-high';
+    }
+
+    let html = `<a href="https://esdoc.org" class="esdoc-coverage-wrap">
+    <span class="esdoc-coverage-label">document</span><span class="esdoc-coverage-ratio ${colorClass}">${coverage}%</span>
+    </a>`;
+
+    return html;
   }
 }
