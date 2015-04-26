aebe0e04f3fa011df13a1f0e65a59067291f403e
Ryo Maruyama <ryo-maruyama@cookpad.com>
2015-04-26 20:59:37 +0900

    feat: document coveraget with each file.

--- src/Publisher/Builder/CoverageBuilder.js
+++ src/Publisher/Builder/CoverageBuilder.js
@@ -6,15 +6,24 @@ export default class CoverageBuilder extends DocBuilder {
     let docs = this._find({kind: ['class', 'method', 'member', 'get', 'set', 'constructor', 'function', 'variable']});
     let expectCount = docs.length;
     let actualCount = 0;
+    let files = {};
 
     for (let doc of docs) {
-      if (!doc.undocument) actualCount++;
+      let filePath = doc.longname.split('~')[0];
+      if (!files[filePath]) files[filePath] = {expectCount: 0, actualCount: 0};
+      files[filePath].expectCount++;
+
+      if (!doc.undocument) {
+        actualCount++;
+        files[filePath].actualCount++;
+      }
     }
 
     let coverage = {
       coverage: `${Math.floor(10000 * actualCount / expectCount) / 100 }%`,
       expectCount: expectCount,
-      actualCount: actualCount
+      actualCount: actualCount,
+      files: files
     };
 
     callback(coverage, 'coverage.json');
--- test/src/DocTest/CoverageDocTest.js
+++ test/src/DocTest/CoverageDocTest.js
@@ -8,5 +8,35 @@ describe('Coverage:', ()=> {
     assert.equal(coverage.coverage, '87.5%');
     assert.equal(coverage.expectCount, 72);
     assert.equal(coverage.actualCount, 63);
+    assert.deepEqual(coverage.files, {
+      "src/MyClass.js": {
+        "expectCount": 35,
+        "actualCount": 30
+      },
+      "src/MyError.js": {
+        "expectCount": 1,
+        "actualCount": 1
+      },
+      "src/MyEvent.js": {
+        "expectCount": 1,
+        "actualCount": 1
+      },
+      "src/MyInterface.js": {
+        "expectCount": 3,
+        "actualCount": 3
+      },
+      "src/OtherClass/SuperMyClass.js": {
+        "expectCount": 19,
+        "actualCount": 19
+      },
+      "src/myFunction.js": {
+        "expectCount": 8,
+        "actualCount": 6
+      },
+      "src/myVariable.js": {
+        "expectCount": 5,
+        "actualCount": 3
+      }
+    });
   });
 });
