7f6fded1bc8fc70c974b130178ce4dabfaee2880
h13i32maru <h13i32maru@gmail.com>
2015-05-01 18:32:22 +0900

    fix: inenr link scroll amount minor adjustment.

--- src/Publisher/Builder/template/script/inner-link.js
+++ src/Publisher/Builder/template/script/inner-link.js
@@ -2,12 +2,12 @@
 // so, I manually scroll.
 (function(){
   window.addEventListener('hashchange', function(evt){
-    window.scrollBy(0, -50);
+    window.scrollBy(0, -55);
   });
 
   if (location.hash) {
     setTimeout(function(){
-      window.scrollBy(0, -50);
+      window.scrollBy(0, -55);
     }, 0);
   }
 })();
