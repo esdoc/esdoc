fe9dc82511a9d461a2019dfd057bca7c244e6a97
h13i32maru <h13i32maru@gmail.com>
2015-05-02 14:50:53 +0900

    feat: highlight line if location hash is exists.

--- src/Publisher/Builder/template/script/pretty-print.js
+++ src/Publisher/Builder/template/script/pretty-print.js
@@ -4,4 +4,9 @@
   for (var i = 0; i < lines.length; i++) {
     lines[i].id = 'lineNumber' + (i + 1);
   }
+
+  if (location.hash) {
+    var line = document.querySelector(location.hash);
+    if (line) line.classList.add('active');
+  }
 })();
