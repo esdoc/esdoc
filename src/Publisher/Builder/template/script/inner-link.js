882fa9c03d3720a5bdfda8b7093949253e00082a
h13i32maru <h13i32maru@gmail.com>
2015-05-04 09:40:05 +0900

    design: when move with inner link, change el background color.

--- src/Publisher/Builder/template/script/inner-link.js
+++ src/Publisher/Builder/template/script/inner-link.js
@@ -1,13 +1,18 @@
 // inner link(#foo) can not correctly scroll, because page has fixed header,
 // so, I manually scroll.
 (function(){
-  window.addEventListener('hashchange', function(evt){
+  function adjust() {
     window.scrollBy(0, -55);
-  });
+    var el = document.querySelector('.inner-link-active');
+    if (el) el.classList.remove('inner-link-active');
+
+    var el = document.querySelector(location.hash);
+    if (el) el.classList.add('inner-link-active');
+  }
+
+  window.addEventListener('hashchange', adjust);
 
   if (location.hash) {
-    setTimeout(function(){
-      window.scrollBy(0, -55);
-    }, 0);
+    setTimeout(adjust, 0);
   }
 })();
