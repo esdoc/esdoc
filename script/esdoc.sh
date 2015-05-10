d80af27eb088a0613d52ea18c4790587181b3abe
h13i32maru <h13i32maru@gmail.com>
2015-05-10 11:29:24 +0900

    chore: remove bash script.

--- script/esdoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./doc/out/esdoc
-mkdir -p ./doc/out/esdoc
-./node_modules/.bin/babel-node ./src/ESDocCLI.js -c esdoc.json
--- script/server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/http-server ./ -p 8080 -c -1
