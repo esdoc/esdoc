402e77e76a22b8ef19bf20fd05dd6524533e95bf
h13i32maru <h13i32maru@gmail.com>
2015-07-05 12:31:47 +0900

    chore: CLI for AST

---
+++ script/ast.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+require('babel/register');
+var ESParser = require('../src/Parser/ESParser.js');
+
+if (!process.argv[2]) {
+  console.log('usage: ast.js path/to/file');
+  process.exit(1);
+}
+
+var ast = ESParser.parse(process.argv[2]);
+console.log(JSON.stringify(ast, null, 2));
