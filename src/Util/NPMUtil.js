9de84b911de3636090ce74fb16cac86a3c06436d
h13i32maru <h13i32maru@gmail.com>
2015-07-20 23:35:48 +0900

    doc: add doc comment.

--- src/Util/NPMUtil.js
+++ src/Util/NPMUtil.js
@@ -1,7 +1,15 @@
 import path from 'path';
 import fs from 'fs-extra';
 
+/**
+ * Node Package Manager(npm) util class.
+ */
 export default class NPMUtil {
+
+  /**
+   * find ESDoc package.json object.
+   * @returns {Object} package.json object.
+   */
   static findPackage() {
     let packageObj = null;
     try {
