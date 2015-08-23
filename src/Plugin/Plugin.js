573a6cf607c62917d23da45aa842edba4458ff9c
h13i32maru <h13i32maru@gmail.com>
2015-08-23 20:53:50 +0900

    fix: add doc tag.

--- src/Plugin/Plugin.js
+++ src/Plugin/Plugin.js
@@ -1,14 +1,31 @@
 import path from 'path';
 
+/**
+ * Plugin system for your plugin.
+ */
 class Plugin {
+  /**
+   * create instance.
+   */
   constructor()  {
     this._plugins = null;
   }
 
+  /**
+   * initialize with plugin property.
+   * @param {Array<{name: string, option: object}>} plugins - expect config.plugins property.
+   */
   init(plugins = []) {
     this._plugins = copy(plugins);
   }
 
+  /**
+   * exec plugin handler.
+   * @param {string} handlerName - handler name(e.g. onHandleCode)
+   * @param {PluginEvent} ev - plugin event object.
+   * @param {boolean} [giveOption=false] - if true, event has plugin option.
+   * @private
+   */
   _execHandler(handlerName, ev, giveOption = false) {
     for (let item of this._plugins) {
       let plugin;
@@ -29,23 +46,41 @@ class Plugin {
     }
   }
 
+  /**
+   * handle start.
+   */
   onStart() {
     const ev = new PluginEvent();
     this._execHandler('onStart', ev, true);
   }
 
+  /**
+   * handle config.
+   * @param {ESDocConfig} config - original esdoc config.
+   * @returns {ESDocConfig} handled config.
+   */
   onHandleConfig(config) {
     const ev = new PluginEvent({config});
     this._execHandler('onHandleConfig', ev);
     return ev.data.config;
   }
 
+  /**
+   * handle code.
+   * @param {string} code - original code.
+   * @returns {string} handled code.
+   */
   onHandleCode(code) {
     const ev = new PluginEvent({code});
     this._execHandler('onHandleCode', ev);
     return ev.data.code;
   }
 
+  /**
+   * handle code parser.
+   * @param {function(code: string)} parser - original js parser.
+   * @returns {function(code: string)} handled parser.
+   */
   onHandleCodeParser(parser) {
     const ev = new PluginEvent();
     ev.data.parser = parser;
@@ -53,31 +88,56 @@ class Plugin {
     return ev.data.parser;
   }
 
+  /**
+   * handle AST.
+   * @param {AST} ast - original ast.
+   * @returns {AST} handled AST.
+   */
   onHandleAST(ast) {
     const ev = new PluginEvent({ast});
     this._execHandler('onHandleAST', ev);
     return ev.data.ast;
   }
 
+  /**
+   * handle tag.
+   * @param {Tag} tag - original tag(s).
+   * @returns {Tag} handled tag.
+   */
   onHandleTag(tag) {
     const ev = new PluginEvent({tag});
     this._execHandler('onHandleTag', ev);
     return ev.data.tag;
   }
 
+  /**
+   * handle HTML.
+   * @param {string} html - original HTML.
+   * @returns {string} handled HTML.
+   */
   onHandleHTML(html) {
     const ev = new PluginEvent({html});
     this._execHandler('onHandleHTML', ev);
     return ev.data.html;
   }
 
+  /**
+   * handle complete
+   */
   onComplete() {
     const ev = new PluginEvent();
     this._execHandler('onComplete', ev);
   }
 }
 
-class PluginEvent {
+/**
+ * Plugin Event class.
+ */
+export class PluginEvent {
+  /**
+   * create instance.
+   * @param {Object} data - event content.
+   */
   constructor(data = {}) {
     this.data = copy(data);
   }
@@ -87,4 +147,7 @@ function copy(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
+/**
+ * Instance of Plugin class.
+ */
 export default new Plugin();
--- src/Util/NamingUtil.js
+++ src/Util/NamingUtil.js
@@ -2,7 +2,15 @@ import path from 'path';
 
 let filePathMap = {};
 
+/**
+ * Identifier Naming Util class.
+ */
 export default class NamingUtil {
+  /**
+   * naming with file path.
+   * @param {string} filePath - target file path.
+   * @returns {string} name
+   */
   static filePathToName(filePath) {
     let basename = path.basename(filePath).split('.')[0];
     basename = basename.replace(/[^a-zA-Z0-9_$]/g, '');
