203fba18b1a1d035fca8e73aca351edb3bcfad68
h13i32maru <h13i32maru@gmail.com>
2015-08-29 17:21:31 +0900

    chore(test): correct istanbul option

--- script/test-es5.js
+++ script/test-es5.js
@@ -4,14 +4,13 @@ var sh = require('./sh');
 // judge node or io because run test with node and io on TRAVIS.
 // send coverage report with only node.
 var isNodeJS = process.version.indexOf('v0.12.') === 0;
-var mochaOption=" -t 10000 --recursive ./out/test/src -R spec";
+var mochaOption = " -t 10000 --recursive ./out/test/src -R spec";
+var istanbulOption = ' cover --include-all-sources --root ./out/src/ -x "template/*" ./node_modules/mocha/bin/_mocha  -- ' + mochaOption;
 
 sh.exec('node ./script/build.js');
 
 if (process.env.TRAVIS && isNodeJS) {
-  sh.exec('./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- ' + mochaOption + ' && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js');
-} else if(process.argv.indexOf('--coverage') !== -1) {
-  sh.exec('./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha  -- ' + mochaOption);
+  sh.exec('./node_modules/.bin/istanbul' + istanbulOption + ' && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js');
 } else {
-  sh.exec('./node_modules/.bin/mocha' + mochaOption);
+  sh.exec('./node_modules/.bin/istanbul' + istanbulOption);
 }
