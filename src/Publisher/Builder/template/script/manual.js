284bc91ca4521cd70896592b49e5e99de9050a52
h13i32maru <h13i32maru@gmail.com>
2015-10-18 21:30:49 +0900

    fix: nit

--- src/Publisher/Builder/template/script/manual.js
+++ src/Publisher/Builder/template/script/manual.js
@@ -1,5 +1,8 @@
 (function(){
-  var currentName = location.pathname.match(/([^/]*)\.html$/)[1];
+  var matched = location.pathname.match(/([^/]*)\.html$/);
+  if (!matched) return;
+
+  var currentName = matched[1];
   var cssClass = '.navigation [data-toc-name="' + currentName + '"]';
   var styleText = cssClass + ' .manual-toc { display: block; }\n';
   styleText += cssClass + ' .manual-toc-title { background-color: #039BE5; }\n';
