b63c9b00c72a3e13abcc804a726649d48268559a
typhonrt <support@typhonrt.org>
2016-06-04 01:30:08 -0700

    fully integrated typhonjs-npm-build-test

--- script/build.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var sh = require('./sh');
-
-sh.rm('./out/src');
-sh.mkdir('./out/src');
-sh.exec('./node_modules/.bin/babel src -d out/src');
-sh.chmod('./out/src/ESDocCLI.js', '755');
-sh.cp('./src/Publisher/Builder/template/', './out/src/Publisher/Builder/template/');
