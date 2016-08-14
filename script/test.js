149914e84cb3bb061f2bdf2e43af00178b131a9e
h13i32maru <h13i32maru@gmail.com>
2016-08-14 13:11:10 +0900

    feat(babel): update babel to 6

--- script/test.js
+++ script/test.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 var sh = require('./sh');
 
-var mochaOption=" -t 10000 --require ./node_modules/babel/register.js --recursive ./test/src -R spec";
+var mochaOption=" -t 10000 --require ./node_modules/babel-register --recursive ./test/src -R spec";
 sh.exec('./node_modules/.bin/mocha' + mochaOption);
