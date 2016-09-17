3bdfae0c24a0f60033b4ea9a8f7ab7b7551fb720
h13i32maru <h13i32maru@gmail.com>
2016-09-17 18:26:38 +0900

    refactor(test): of invalid

---
+++ test/fixture/src/Invalid/DocSyntax.js
@@ -0,0 +1,4 @@
+/**
+ * @param {} p
+ */
+function testInvalidCodeSyntax(p){}
---
+++ test/src/DocTest/InvalidTest.js
@@ -0,0 +1,16 @@
+import assert from 'assert';
+import InvalidCodeLogger from '../../../src/Util/InvalidCodeLogger';
+
+/** @test {InvalidCodeLogger} */
+describe('test invalid code', ()=>{
+  it('has invalid code log.', ()=>{
+    console.log(InvalidCodeLogger._logs);
+    assert.equal(InvalidCodeLogger._logs.length, 2);
+
+    assert(InvalidCodeLogger._logs[0].filePath.includes('test/fixture/src/Invalid/CodeSyntax.js'));
+    assert.deepEqual(InvalidCodeLogger._logs[0].log, [1, 2]);
+
+    assert(InvalidCodeLogger._logs[1].filePath.includes('test/fixture/src/Invalid/DocSyntax.js'));
+    assert.deepEqual(InvalidCodeLogger._logs[1].log, [1, 4]);
+  });
+});
