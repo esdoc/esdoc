e2b88219d584a0f74efbe296d70094d1bae0290e
h13i32maru <h13i32maru@gmail.com>
2016-09-19 00:54:31 +0900

    feat(test): add help and version test

--- test/src/ConfigTest/CLITest.js
+++ test/src/ConfigTest/CLITest.js
@@ -1,12 +1,39 @@
-import {assert, cli} from '../util.js';
+import path from 'path';
+import {consoleLogSwitch} from '../util.js';
+import ESDocCLI from '../../../src/ESDocCLI.js';
 
 /**
  * @test {ESDocCLI#exec}
  * @test {ESDocCLI#_createConfigFromJSONFile}
  */
 describe('test cli', ()=>{
+  const cliPath = path.resolve('./src/cli.js');
+  const configPath = path.resolve('./test/fixture/config/esdoc-cli.json');
+
   it('can execute with config file.', ()=>{
-    cli('./test/fixture/config/esdoc-cli.json');
-    assert(true);
+    const argv = ['node', cliPath, '-c', configPath];
+    const cli = new ESDocCLI(argv);
+
+    consoleLogSwitch(false);
+    cli.exec();
+    consoleLogSwitch(true);
+  });
+
+  it('can show help', ()=>{
+    const argv = ['node', cliPath, '-c', configPath];
+    const cli = new ESDocCLI(argv);
+
+    consoleLogSwitch(false);
+    cli._showHelp();
+    consoleLogSwitch(true);
+  });
+
+  it('can show version', ()=>{
+    const argv = ['node', cliPath, '-c', configPath];
+    const cli = new ESDocCLI(argv);
+
+    consoleLogSwitch(false);
+    cli._showVersion();
+    consoleLogSwitch(true);
   });
 });
