c20caf86a195a7a96770c961fdc5f3bea205f587
h13i32maru <h13i32maru@gmail.com>
2016-09-18 22:41:37 +0900

    feat(test): add config.test test

---
+++ test/src/UnitTest/ConfigTest/TestTest.js
@@ -0,0 +1,18 @@
+import {readDoc as _readDoc, assert, cli, consoleLogSwitch} from '../../util.js';
+
+/** @test {publish} */
+describe('test config.test: null', ()=>{
+  consoleLogSwitch(false);
+  cli('./test/fixture-config/esdoc-test.json');
+  consoleLogSwitch(true);
+
+  function readDoc(filePath) {
+    return _readDoc(filePath, './test/fixture-config/esdoc-test');
+  }
+
+  it('does not have test integration', ()=>{
+    assert.throws(()=>{
+      readDoc('test.html');
+    });
+  });
+});
