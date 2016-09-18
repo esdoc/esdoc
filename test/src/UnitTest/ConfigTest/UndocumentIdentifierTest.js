cbc5a7892f9d2288b3a800d205f43d2fc364a037
h13i32maru <h13i32maru@gmail.com>
2016-09-18 22:46:05 +0900

    feat(test): add config.undocumentIdentifier test

---
+++ test/src/UnitTest/ConfigTest/UndocumentIdentifierTest.js
@@ -0,0 +1,18 @@
+import {readDoc as _readDoc, assert, cli, consoleLogSwitch} from '../../util.js';
+
+/** @test {DocResolver#_resolveUndocumentIdentifier} */
+describe('test config.undocumentIdentifier: false', ()=>{
+  consoleLogSwitch(false);
+  cli('./test/fixture-config/esdoc-undocumentIdentifier.json');
+  consoleLogSwitch(true);
+
+  function readDoc(filePath) {
+    return _readDoc(filePath, './test/fixture-config/esdoc-undocumentIdentifier');
+  }
+
+  it('does not have undocument identifier', ()=>{
+    assert.throws(()=>{
+      readDoc('class/src/Undocument/Definition.js~TestUndocumentDefinition.html');
+    });
+  });
+});
