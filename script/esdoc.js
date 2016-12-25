85c866c75b7b6d3149cac012d5c716f63803f309
h13i32maru <h13i32maru@gmail.com>
2016-12-25 16:27:05 +0900

    fix(site): replace to new site image

--- script/esdoc.js
+++ script/esdoc.js
@@ -3,4 +3,4 @@ const sh = require('./sh');
 
 sh.rm('./doc/out/esdoc');
 sh.mkdir('./doc/out/esdoc');
-sh.exec('./node_modules/.bin/babel-node ./src/ESDocCLI.js -c esdoc.json');
+sh.exec('./node_modules/.bin/babel-node ./src/ESDocCLI.js');
