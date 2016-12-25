23a4285e81c56d71134a229b17581b30de3bb65c
h13i32maru <h13i32maru@gmail.com>
2016-12-25 21:46:13 +0900

    fix: nits

--- script/esdoc.js
+++ script/esdoc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const sh = require('./sh');
 
-sh.rm('./doc/out/esdoc');
-sh.mkdir('./doc/out/esdoc');
+sh.rm('./out/esdoc');
+sh.mkdir('./out/esdoc');
 sh.exec('./node_modules/.bin/babel-node ./src/ESDocCLI.js');
