a98a83c2cdb2904e2f8a246dee8cf886da47fc48
h13i32maru <h13i32maru@gmail.com>
2016-12-26 09:11:14 +0900

    fix(invalid): message

--- src/Util/InvalidCodeLogger.js
+++ src/Util/InvalidCodeLogger.js
@@ -57,7 +57,7 @@ class InvalidCodeLogger {
       targetLines.push(`${i + 1}| ${lines[i]}`);
     }
 
-    console.log('[31mwarning: could not parse the following code. if you want to use ES7, see esdoc-es7-plugin(https://github.com/esdoc/esdoc-es7-plugin)[32m');
+    console.log('[31mwarning: could not parse the following code. if you want to use ECMAScript proposals, see https://esdoc.org/manual/usage/feature.html#ecmascript-proposal[32m');
     console.log(filePath);
     console.log(`${targetLines.join('\n')}[0m`);
 
