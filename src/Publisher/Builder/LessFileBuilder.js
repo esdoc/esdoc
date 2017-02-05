90ae0e5ccf3156f51131bc9d0337fa5453abbc44
Stéphane.francel <s.francel@avine.fr>
2017-02-05 07:36:43 +0100

    easly add user theme in the Less process - in .esdoc.json less.theme might an alias relative to /less/theme or a path to a user less file - in .esdoc.json less.imports.variables is optional (will be prepended automatically)

--- src/Publisher/Builder/LessFileBuilder.js
+++ src/Publisher/Builder/LessFileBuilder.js
@@ -14,18 +14,19 @@ export default class LessFileBuilder extends DocBuilder {
     console.log('output: ./css/style.css');
     console.log('output: ./css/style.css.map');
 
-    // set theme filename
-    const theme = (this._config.less || {}).theme;
-    this.theme = theme ? `theme-${theme}.less` : null;
+    this._setPath();
 
-    // set path to /less, /css, and relative from /css to /less
-    const input = path.resolve(__dirname, './template/less');
-    const output = path.resolve(this._config.destination, './css');
-    const relative = path.relative(output, input);
-    this.path = {input, output, relative};
-
-    if (this.theme) {
-      fs.readFile(`${this.path.input}/${this.theme}`, 'utf8', (err, data) => {
+    // check theme
+    let theme = (this._config.less || {}).theme;
+    if (theme) {
+      if (/^[a-z0-9\-_]+$/i.test(theme)) {
+        // theme is a file alias relative to `/less/theme` directory
+        theme = path.resolve(this.path.input, `./theme/${theme}.less`);
+      } else {
+        // otherwise theme is a real path to filename
+        theme = path.resolve(theme);
+      }
+      fs.readFile(theme, 'utf8', (err, data) => {
         if (err) throw err;
         this._lessRender(data);
       });
@@ -35,51 +36,64 @@ export default class LessFileBuilder extends DocBuilder {
   }
 
   /**
+   * set path to `/less`, `/css`, and relative from `/css` to `/less`.
+   * @private
+   */
+  _setPath() {
+    const input = path.resolve(__dirname, './template/less');
+    const output = path.resolve(this._config.destination, './css');
+    const relative = path.relative(output, input);
+    this.path = {input, output, relative};
+  }
+
+  /**
    * render less to css.
    * @param {string} data - less content of the theme file.
    * @private
    */
   _lessRender(data = '') {
     const imports = this._buildImports();
-    const paths = [this.path.input];
-    const filename = this.theme || 'style.less';
-
     less.render(`${imports}\n${data}`, {
-      paths: paths,       // Specify search paths for @import directives
-      filename: filename, // Specify a filename, for better error messages
-      compress: false,    // Minify CSS output
+      paths: [this.path.input], // Specify search paths for @import directives
+      filename: 'style.less',   // Specify a filename, for better error messages
+      compress: false,          // Minify CSS output
       sourceMap: {
         sourceMapURL: 'style.css.map',
         outputSourceFiles: true
       }
     }, (err, output) => {
       if (err) throw err;
-      this._outputCss(output.css);
-      this._outputMap(output.map);
+      if (output.map) {
+        this._outputCss(output.css);
+        this._outputMap(output.map);
+      } else {
+        // if the map is undefined then there's no style to output
+        this._outputCss();
+      }
     });
   }
 
   /**
-   * write style.css to the destination.
+   * write style.css to destination.
    * @param {string} css - css content.
    * @private
    */
-  _outputCss(css) {
-    fs.writeFile(`${this.path.output}/style.css`, css, 'utf8', (err) => {
+  _outputCss(css = '') {
+    fs.writeFile(path.resolve(this.path.output, './style.css'), css, 'utf8', (err) => {
       if (err) throw err;
     });
   }
 
   /**
-   * write style.css.map to the destination.
+   * write style.css.map to destination.
    * @param {string} map - sourcemap.
    * @private
    */
-  _outputMap(map) {
+  _outputMap(map = '') {
     const absolute = new RegExp(this.path.input, 'g');
     map = map.replace(absolute, this.path.relative);
 
-    fs.writeFile(`${this.path.output}/style.css.map`, map, 'utf8', (err) => {
+    fs.writeFile(path.resolve(this.path.output, './style.css.map'), map, 'utf8', (err) => {
       if (err) throw err;
     });
   }
@@ -89,8 +103,9 @@ export default class LessFileBuilder extends DocBuilder {
    * @private
    */
   _buildImports() {
+    // available less files in `/less/core` directory
+    // note: `variables` is not listed here (see below)
     const available = [
-      'variables',
       'text',
       'layout',
       'menu',
@@ -109,6 +124,11 @@ export default class LessFileBuilder extends DocBuilder {
         less.push(`@import "core/${item}";`);
       }
     });
+    if (less.length) {
+      // if at least one less file is required
+      // then automatically prepend `variables`
+      less.unshift('@import "core/variables";');
+    }
     return less.join('\n');
   }
 
