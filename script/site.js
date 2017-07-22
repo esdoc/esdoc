e04865a6aa603b8f05b92fb44c69ec6711ade9a8
h13i32maru <h13i32maru@gmail.com>
2017-07-23 08:40:50 +0900

    fix(site): remove

--- script/site.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const sh = require('./sh');
-
-sh.rm('./out/site');
-sh.mkdir('./out/site');
-sh.exec('./node_modules/.bin/babel-node ./src/ESDocCLI.js -c ./site/esdoc-site.json');
