5ccd3263d1f4602d257c1b44261b9bfa0bd26ed1
jared.fox <jared.fox@kroger.com>
2017-11-07 17:06:17 -0500

    Issue #406 | refactor test init.js, bipass ESDocCLI to use ESDoc

--- test/init.js
+++ test/init.js
@@ -1,28 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import ESDocCLI from '../src/ESDocCLI.js';
+import fs from 'fs'
+import path from 'path'
+import ESDoc from '../src/ESDoc.js'
 
-export function cli(configPath) {
-  const cliPath = path.resolve('./src/cli.js');
-  const argv = ['node', cliPath];
-
-  if (configPath) {
-    configPath = path.resolve(configPath);
-    argv.push('-c', configPath);
-    console.log(`== start == ${configPath}`);
-  }
-
-  const cli = new ESDocCLI(argv);
-
-  return cli.exec()
+export function generateDocs (configPath) {
+  return ESDoc.generate(require(path.resolve(configPath)))
 }
 
-export default cli('./test/integration-test/esdoc.json').then(() => {
-  console.log(`== finish ==`);   
-  global.docs = JSON.parse(fs.readFileSync('./test/integration-test/out/index.json').toString());  
+export default generateDocs('./test/integration-test/esdoc.json').then(() => {
+  console.log(`== finish ==`)
+  global.docs = JSON.parse(fs.readFileSync('./test/integration-test/out/index.json').toString())
 }).catch((e) => {
-  console.log('CATCH CLI CATCH ', e);
-});
-
-
-
+  console.log('== init.js | ESDoc.generate | catch ==', e)
+})
