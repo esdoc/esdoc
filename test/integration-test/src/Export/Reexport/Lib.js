b820e5f15c00143f7a73888191624f21702bb054
Duarte David <deltaduartedavid@gmail.com>
2018-05-21 02:38:30 +0200

    test(new): add tests

---
+++ test/integration-test/src/Export/Reexport/Lib.js
@@ -0,0 +1,10 @@
+const a = 1;
+const b = 2;
+const c = 3;
+const d = 4;
+export {
+	a,
+	b,
+	c
+};
+export default d;
---
+++ test/integration-test/src/Export/Reexport/Reexport.js
@@ -0,0 +1,8 @@
+import d, { a, b, c } from './Lib.js'
+
+export {
+	a,
+	b,
+	c,
+	d
+}
